b923ce535f65c96f5f6bca0ef687d180604a0d39
Zan Wills <wwills2@umbc.edu>
2024-01-13 07:04:39 -0500

    updated cmake file and build process

--- test/TestLinearSystem.h
+++ test/TestLinearSystem.h
@@ -7,8 +7,8 @@
 
 #include <random>
 #include <algorithm>
-#include "../src/libmatrix/MatrixManager.h"
-#include "../src/libmatrix/Matrix.h"
+#include "../libmatrix/MatrixManager.h"
+#include "../libmatrix/Matrix.h"
 
 #define LOG_FILE "./test_output.txt"
 class Random;
